966a9f5131905ba0798ebdf44057a29888c83183
Od@ <elmorochez22@gmail.com>
2017-01-18 08:33:06 -0400

    nueva documentacion

--- app/partidas/controllers/Principal.php
+++ app/partidas/controllers/Principal.php
@@ -27,18 +27,6 @@ class Principal extends PrincipalController
 		$this->view('partidas/index');
     }
 
-	/**
-	* A summary informing the user what the associated element does.
-	*
-	* A *description*, that can span multiple lines, to go _in-depth_ into the details of this element
-	* and to provide some background information or textual references.
-	*
-	* @param string $myArgument With a *description* of this argument, these may also
-	*    span multiple lines.
-	*
-	* @return void
-	*/
-
     public function dos()
     {
     	echo PrincipalModel::dos();
