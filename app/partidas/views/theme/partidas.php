154103a4f09f9284c3118c461c93378fd858efb3
Od@ <elmorochez22@gmail.com>
2017-01-23 10:31:39 -0400

    modificado el theme para usar los -baseUrl- de las variables del entorno que se formatean en el principio de la aplicacion

--- app/partidas/views/theme/partidas.php
+++ app/partidas/views/theme/partidas.php
@@ -3,14 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>partidas</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<?php $baseUrl = baseUrl ?>
+	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/bootstrap/dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/components-font-awesome/css/font-awesome.min.css">
 </head>
 <body>
 <br>
 <div class="container">
 <?php echo $content ?>
 </div>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="<?php echo baseUrl ?>assets/bower/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ Configuraciones Generales del Sistema
 --------------------------------------------------------------*/ 
 
 /* Url base del Sistema */
-$baseUrl = $_SERVER['ENV_BASE_URL'];
+$baseUrl = 'http://'.$_SERVER['ENV_BASE_URL'];
 
 /* Ruta de la Clase Default */
 $ClaseDefault = $_SERVER['ENV_CLASE_DEFAULT'];
