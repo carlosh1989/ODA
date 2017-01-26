819c1ec22c500a9ad27367d0478705209116897c
Od@ <elmorochez22@gmail.com>
2017-01-26 10:23:17 -0400

    generando archivos de prueba

--- resources/templateGlobal/default.php
+++ resources/templateGlobal/default.php
@@ -6,12 +6,16 @@
 	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/bootstrap/dist/css/bootstrap-theme.min.css">
 	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/components-font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo baseUrl ?>assets/bower/sweetalert/dist/sweetalert.css">
 </head>
 <body>
 <br>
 <div class="container">
-<?php echo $content ?>
+	<?php echo $content ?>
 </div>
+    <script src="<?php echo baseUrl ?>assets/bower/jquery/dist/jquery.min.js"></script>
 	<script src="<?php echo baseUrl ?>assets/bower/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo baseUrl ?>assets/bower/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="<?php echo baseUrl ?>bower install jquery-mask-plugin"></script>
 </body>
 </html>
